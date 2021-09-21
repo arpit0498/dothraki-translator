810967c65b8c729a7d3e306ad8f7ef6113ce4732
arpit gupta <mycontact.arpit@gmail.com>
2021-09-21 17:27:09 +0530

    error fixes

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-var btnTranslate =document.querySelector('#btn-translate');
-var txtInput =document.querySelector("#txt-input");
-var outputDiv =document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
 
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
 
-var serverURL="https://funtranslations.com/api/dothraki" ;
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
+};
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" +text ;
-}
-function errorHandler(error){
-    console.log("error occured", error);
+function errorHandling(error) {
+    console.log("error occured!", error);
     alert("something went wrong")
 };
 
-function clickHandler(){
-     var inputText = txtInput.value;
-
-     fetch(getTranslationURL(inputText))
-     .then(response => response.json()) 
-     .then(json => {
-         var translatedText =json.contents.translated;
-         outputDiv.innerText = translatedText ;
-     })
-     .catch(errorHandler)
+function translateAction() {
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(errorHandling)
 };
 
+btnTranslate.addEventListener("click", translateAction);
 
-
-btnTranslate.addEventListener("click", clickHandler)
   
